0f923bd42419d84212a6752205821df883195a65
Maxim Obelets <maximobelets@gmail.com>
2019-06-17 14:56:35 +0500

    Fixing styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from './Button';
+import './App.css';
 
 const App = () => {
     return(
         <main>
+            <h4>Simply React Counter</h4>
             <Button />
         </main>
     );
