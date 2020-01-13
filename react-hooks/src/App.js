e5e48800deda4063d997ea38c15c1fad056dc593
Maxim Obelets <maximobelets@gmail.com>
2020-01-13 16:42:49 +0500

    added styles

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import './app.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h4>Counter</h4>
+    <section>
+      <h4>React Hooks Counter</h4>
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
       <button onClick={() => setCount(Math.floor(Math.random() * 100))}>Random</button>
-    </div>
+    </section>
   );
 };
 
