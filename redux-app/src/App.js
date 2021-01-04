2ffe0bbaaac4047eb2be50b06b4b31aa5bf5055d
Maxim Obelets <maximobelets@gmail.com>
2021-01-04 10:04:45 +0500

    Add redux logic

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,9 +1,28 @@
-function App() {
-  return (
-    <div className="App">
-      Redux
-    </div>
-  );
+import React from 'react';
+import { connect } from 'react-redux';
+import { inc, dec } from '../src/redux/actions/actions';
+
+const App = (props) => {
+	return (
+		<React.Fragment>
+			<h3>{props.count}</h3>
+			<button onClick={props.inc}>+</button>
+			<button onClick={props.dec}>-</button>
+		</React.Fragment>
+	);
+};
+
+const mapStateToProps = (state) => {
+	return {
+		count: state.count
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		inc: () => dispatch(inc()),
+		dec: () => dispatch(dec()),
+	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
